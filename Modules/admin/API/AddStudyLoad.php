a492c0bdc91fb307745e11d048a993d290cd2384
Медведев Константин <kostj1998.10.13@yandex.ru>
2021-04-10 10:52:28 +0300

    Добавлен механизм обновления записей

--- Modules/admin/API/AddStudyLoad.php
+++ Modules/admin/API/AddStudyLoad.php
@@ -16,6 +16,8 @@ $AGCode =         $_POST['AGCode'];
 $NumberHours =    $_POST['NumberHours'];
 $AdditionalLoad = $_POST['AdditionalLoad'];
 
+if($_POST['isUpdateStudyLoad'] == 'NO')
+{
 $AddQuery = 'Insert into studyload(SL_DISC_id, SL_TypeLesson_code, SL_NumberHours, SL_Teacher_id, SL_AcademGroup_code, SL_AdditionalLoad) 
 value(:Discipline, :TypeLesson, :NumberHours, :Teacher, :AGCode, :AdditionalLoad)';
 
@@ -45,4 +47,42 @@ try {
 catch (PDOException $exception)
 {
     exit("<p class='alert alert-danger'>Exception: ".$exception->getCode().". В процессе добавление записи возникла критическая ошибка. Вероятнее всего такая запись уже есть в базе. Если это не так и ошибка повторяется - обратитесь к администратору.</p>");
+}
+    }
+else{
+
+    $id = $_POST['isUpdateStudyLoad'];
+
+    $UpdateQuery = "Update jagu.studyload set 
+                          SL_DISC_id = :Discipline, 
+                          SL_TypeLesson_code = :TypeLesson,
+                          SL_NumberHours = :NumberHours, 
+                          SL_Teacher_id = :Teacher, 
+                          SL_AdditionalLoad = :AdditionalLoad 
+where SL_Id = :id";
+
+    try{
+        $db = db_connect();
+
+        $stmp = $db->prepare($UpdateQuery);
+
+        $UpdateResult = $stmp->execute([
+            "Discipline"=>$Discipline,
+            "TypeLesson"=>$TypeLesson,
+            "NumberHours"=>$NumberHours,
+            "Teacher"=>$Teacher,
+            "AdditionalLoad"=>$AdditionalLoad,
+            "id"=>$id,
+        ]);
+
+        if($UpdateResult)
+            exit('<p class="alert alert-success">Нагрузка успешно обновлена!</p>');
+        else
+            exit('<p class="alert alert-danger">Неудалось обновить информацию.</p>');
+    }
+    catch (PDOException $exception)
+    {
+        exit("<p class='alert alert-danger'>Exception: ".$exception->getCode().". В процессе обновления записи возникла критическая ошибка. Проверьте правильность заполнения полей или повторите операцию пзже. Если ошибка повторяется - обратитесь к администратору.</p>");
+
+    }
 }
