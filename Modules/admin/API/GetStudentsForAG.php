ae04f018763d6f7819c938254ec7504c163d96b5
Медведев Константин <kostj1998.10.13@yandex.ru>
2021-04-13 12:45:05 +0300

    Add: выборка студентов, невходящих в подгруппу.

--- Modules/admin/API/GetStudentsForAG.php
+++ Modules/admin/API/GetStudentsForAG.php
@@ -11,7 +11,7 @@ if(!isset($_POST['AGCode']) && !isset($_POST['SGID']))
 require_once($_SERVER['DOCUMENT_ROOT'].'/cfg/core.php');
 
 // Выборка всех студентов группы
-if(isset($_POST['AGCode'])) {
+if(isset($_POST['AGCode']) And !isset($_POST['isNotSG'])) {
     $AGCode = $_POST['AGCode'];
 
     $query = "Select SP_id as ID,
@@ -59,6 +59,56 @@ from studentprofile where SP_AcademGroup_id = :AGCode";
     }
 }
 
+
+//Выборка всех студентов группы AGCode, не входящих в SGID подгруппу
+
+if(isset($_POST['isNotSG']) == true And isset($_POST['AGCode']) And isset($_POST['SGID']))
+{
+    $AGCode = $_POST['AGCode'];
+    $SGID = $_POST['SGID'];
+    $query = "Select SP_id As ID,
+            CONCAT(SP_Surname, ' ', SP_Name, ' ', SP_MiddleName) As FIO
+       from studentprofile 
+left join studentlistinsubgroups s on studentprofile.SP_id = s.SLS_Student_id
+where SP_AcademGroup_id = :AGCode AND (s.SLS_SubGroups_id != :SGID Or s.SLS_SubGroups_id is null )";
+
+    try{
+        $db = db_connect();
+        $stmp = $db->prepare($query);
+        $stmp->execute([
+            'AGCode'=>$AGCode,
+            'SGID'=>$SGID
+        ]);
+
+        $Result = $stmp->fetchAll(PDO::FETCH_ASSOC);
+
+        $json = array();
+
+        foreach ($Result as $value)
+            $json[] = array(
+                '1' =>  $value['FIO'],
+                '2' => $value['ID']
+            );
+
+        $RESPONSE_AJAX['data']= $json;
+        $RESPONSE_AJAX['success']=true;
+
+        exit (json_encode($RESPONSE_AJAX));
+
+    }
+    catch (PDOException $exception)
+    {
+       $RESPONSE_AJAX['success']=false;
+       $RESPONSE_AJAX['ErrorInfo']=$exception->getMessage();
+       exit($RESPONSE_AJAX);
+    }
+
+
+
+}
+
+
+
 // Выборка всех студентов подгруппы
 if(isset($_POST['SGID']))
 {
@@ -97,5 +147,6 @@ if(isset($_POST['SGID']))
     {
         $RESPONSE_AJAX['success']=false;
         $RESPONSE_AJAX['ErrorInfo'] = $exception->getMessage();
+        exit($RESPONSE_AJAX);
     }
 }
